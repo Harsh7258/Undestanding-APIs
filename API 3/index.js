7fb1be9cd91346d8a433412c29335bef8957d9fd
K Harsh <harshnaidu2u@gmail.com>
2024-02-08 13:01:31 +0530

    scrapping news from mutliple sites

--- API 3/index.js
+++ API 3/index.js
@@ -4,40 +4,98 @@ const cheerio = require('cheerio');
 
 const app = express();
 
+const newspapers = [
+    {
+        name: 'guardian',
+        url: 'https://www.theguardian.com/technology/cryptocurrencies'
+    },
+    {
+        name: 'crpyto',
+        url: 'https://crypto.news/'
+    },
+    {
+        name: 'Times-of-India',
+        url: 'https://timesofindia.indiatimes.com/business/cryptocurrency'    
+    },
+    {
+        name: 'economics-times',
+        url: 'https://economictimes.indiatimes.com/topic/indian-cryptocurrency'
+    }
+];
+
 const articles = [];
+
+newspapers.forEach((newspaper) => {
+    axios.get(newspaper.url).then((response) => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+
+        $('a:contains("Bitcoin")', html).each(function() {
+            const title = $(this).text();
+            const url = $(this).attr('href');
+
+            articles.push({
+                title,
+                url,
+                source: newspaper.name
+            })
+        })
+    })
+})
 
 app.get('/', (req, res) => {
     // console.log("GET route!!");
 
     res.status(200).json({
         status: 'success',
-        message: 'World wide football news API.'
+        message: 'Bitcoin news across different news websites.\n Please use this exact names for rotuing:-\n 1. guardian\n 2. crpyto\n 3. Times-of-India\n 4. economics-times'
     })
 });
 
 app.get('/news', (req, res) => {
-    axios.get('https://www.goal.com/en-in/news').then((response) => {
-        const html = response.data;
-        // console.log(JSON.stringify(html));
-        const $ = cheerio.load(html);
-
-        $('a:contains("Cristiano Ronaldo")', html).each(function() {
-            const title = $(this).text();
-            const url = $(this).attr('href');
-
-            articles.push({
-                title,
-                url
-            })
-        })
+    try {
         res.status(200).json({
             status: 'success',
             data: {
                 articles
             }
         })
-    }).catch((err) => console.log(err)); 
-})
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.get('/news/:newspaperId', async (req, res) => {
+    // console.log(req.params.newspaperId);
+    const newspaperIdName = req.params.newspaperId;
+    // console.log(newspaperName);
+
+    const newspaperURL = newspapers.filter((news) => news.name == newspaperIdName)[0].url
+    // console.log(newspaperURL);
+
+    axios.get(newspaperURL).then((response) => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const specifiedArticles = [];
+
+        $('a:contains("Bitcoin")', html).each(function() {
+            const title = $(this).text();
+            const url = $(this).attr('href');
+
+            specifiedArticles.push({
+                title,
+                url,
+                source: newspaperIdName
+            })
+        })
+        res.status(200).json({
+            status: 'success',
+            data: {
+                specifiedArticles
+            }
+        })
+    }).catch((err) => console.log(err))
+});
 
 const PORT = 8000;
 app.listen(PORT, () => {
