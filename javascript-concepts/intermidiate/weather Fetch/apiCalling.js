6dd6b71f7aca6ea88611315ad9ad5c0b4c6cc64c
K Harsh <harshnaidu2u@gmail.com>
2024-03-06 15:38:52 +0530

    localStorage, current location

--- javascript-concepts/intermidiate/weather Fetch/apiCalling.js
+++ javascript-concepts/intermidiate/weather Fetch/apiCalling.js
@@ -1,8 +1,12 @@
 const search = document.getElementById("search");
 const searchBtn = document.getElementById("search-btn");
+const getLocation = document.getElementById("get-btn");
+
+const photo = document.getElementById("img-container");// Target a specific element for insertion
 const cityName = document.getElementById("city-name");
 const cityTemp = document.getElementById("city-temp");
 const cityTime = document.getElementById("city-time");
+const cityData = document.getElementById("city-data");
 
 async function getAPIData(cityname) {
     const api = await fetch(`http://api.weatherapi.com/v1/current.json?key=ccee86e9ac8345b09ab172616240503&q=${cityname}&aqi=yes`);
@@ -16,5 +20,32 @@ searchBtn.addEventListener('click', async () => {
     // console.log(result)
     cityName.innerText = `${result.location.name}, ${result.location.region} - ${result.location.country}`;
     cityTime.innerText = result.location.localtime;
-    cityTemp.innerText = `${result.current.temp_c}C & ${result.current.temp_f}F`
+    cityTemp.innerText = `${result.current.temp_c}C & ${result.current.temp_f}F`;
+
+    // LOCALSTORAGE in javascript
+    localStorage.setItem("city-name", value);
+    // location.reload();
+    const localValue = localStorage.getItem("city-name");
+    cityData.innerText = localValue;
+
+    const imgElement = document.createElement("img");
+      imgElement.src = result.current.condition.icon;
+      imgElement.alt = "Image from API";  // Set an appropriate alt text for accessibility
+
+      photo.innerHTML = ""; // Clear container to ensure only one image
+      photo.appendChild(imgElement);
+});
+
+// GET cuurent location: lan, lon
+function getLoc(position){
+    const currentLoc = document.getElementById("current-loc");
+    currentLoc.innerText = `Lat: ${position.coords.latitude}, Lon: ${position.coords.longitude}`;
+    // console.log(position);
+};
+function failedLoc(err) {
+    console.log("There is something wrong", err);
+};
+
+getLocation.addEventListener("click", async () => {
+    navigator.geolocation.getCurrentPosition(getLoc, failedLoc);
 });
